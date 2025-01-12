3d266c3cfed66830e715a1eff931774168707188
Ryu204 <nguyenanhbao2356@gmail.com>
2025-01-12 11:06:51 +0700

    Feat: more menu buttons (p1)

--- src/scenes/menuScene.ts
+++ src/scenes/menuScene.ts
@@ -1,13 +1,17 @@
 import { Scene } from "phaser";
-import { keys, scenes } from "../constants";
+import { fonts, keys, scenes, texts } from "../constants";
 import { centerOnCamera, setBackground } from "../utils/layout";
 import { createIconButton } from "../components/button";
 import LayeredMusic from "../components/layeredMusic";
 import assets, { Icon } from "../assets";
 import { ButtonColor, ButtonType } from "../assets/ui/buttons";
+import { Modal } from "../components/modal";
+
+type ModalType = "settings" | "highscore" | "howToPlay";
 
 export default class MenuScene extends Scene {
   private music!: LayeredMusic;
+  private modals!: Record<ModalType, Modal>;
 
   constructor() {
     super({ key: scenes.menu });
@@ -26,6 +30,7 @@ export default class MenuScene extends Scene {
       .setOrigin(0.5);
     title.setScale((this.scale.width * 0.8) / title.width);
 
+    this.modals = this.createModals();
     this.createButtons();
     this.setupSceneEvent();
   }
@@ -49,9 +54,7 @@ export default class MenuScene extends Scene {
       this,
       ButtonType.icon,
       { texture: Icon.settings, scale: 0.5 },
-      () => {
-        this.scene.switch(scenes.game);
-      },
+      this.modals.settings.show.bind(this.modals.settings),
       ButtonColor.purple,
       0.6
     );
@@ -59,9 +62,7 @@ export default class MenuScene extends Scene {
       this,
       ButtonType.icon,
       { texture: Icon.rankings, scale: 0.5 },
-      () => {
-        this.scene.switch(scenes.game);
-      },
+      this.modals.highscore.show.bind(this.modals.highscore),
       ButtonColor.green,
       0.7
     );
@@ -69,9 +70,7 @@ export default class MenuScene extends Scene {
       this,
       ButtonType.icon,
       { texture: Icon.question, scale: 0.7 },
-      () => {
-        this.scene.switch(scenes.game);
-      },
+      this.modals.howToPlay.show.bind(this.modals.howToPlay),
       ButtonColor.blue,
       0.6
     );
@@ -87,5 +86,35 @@ export default class MenuScene extends Scene {
       startButton.x + spacing,
       this.scale.height - 200
     );
+  }
+
+  private createModals(): Record<ModalType, Modal> {
+    const [width, height] = [700, 800];
+    const howToPlayText = `1. Swipe to attack
+
+2. Enemies can dodge attack while attacking
+
+3. Get the most score before time runs out`;
+    const howToPlay = [
+      this.add
+        .text(0, 0, howToPlayText, {
+          fontFamily: fonts.primary,
+          fontSize: 46,
+          color: texts.colors.black,
+          wordWrap: { width: width - 100 },
+        })
+        .setOrigin(0.5, 0.5),
+    ];
+
+    const result = {
+      settings: new Modal(this, [], width, height),
+      highscore: new Modal(this, [], width, height),
+      howToPlay: new Modal(this, howToPlay, width, 620),
+    };
+    Object.values(result).forEach((modal) => {
+      modal.setVisible(false);
+      centerOnCamera(modal, this.cameras.main);
+    });
+    return result;
   }
 }
